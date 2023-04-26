a518d0413f64d834fe0f7068881b925f0938b959
julio-valim <juliopvalim@gmail.com>
2023-04-26 16:16:10 -0300

    create project source code

---
+++ src/main/java/com/example/CourseMarketplace/CourseMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.CourseMarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseMarketplaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CourseMarketplace/CourseMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CourseMarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
