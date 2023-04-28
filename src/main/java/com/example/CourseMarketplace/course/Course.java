8b9ea5843f3c1efce0b853178b173da9cfa89680
julio-valim <juliopvalim@gmail.com>
2023-04-28 15:28:52 -0300

    add constructor to course object classs

--- src/main/java/com/example/CourseMarketplace/course/Course.java
+++ src/main/java/com/example/CourseMarketplace/course/Course.java
@@ -1,13 +1,27 @@
 package com.example.CourseMarketplace.course;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Table(name = "courses")
 @Entity(name = "courses")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Course {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String cover;
     private Integer price;
+
+    public Course(CourseRequestDTO data){
+        this.title = data.title();
+        this.cover = data.cover();
+        this.price = data.price();
+    }
 }
