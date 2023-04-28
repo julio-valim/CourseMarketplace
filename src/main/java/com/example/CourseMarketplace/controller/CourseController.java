a94c8b51c28b9f3adad9217d44452ac8f7575967
julio-valim <juliopvalim@gmail.com>
2023-04-28 15:30:01 -0300

    update with dto; add post method

--- src/main/java/com/example/CourseMarketplace/controller/CourseController.java
+++ src/main/java/com/example/CourseMarketplace/controller/CourseController.java
@@ -2,10 +2,10 @@ package com.example.CourseMarketplace.controller;
 
 import com.example.CourseMarketplace.course.Course;
 import com.example.CourseMarketplace.course.CourseRepository;
+import com.example.CourseMarketplace.course.CourseRequestDTO;
+import com.example.CourseMarketplace.course.CourseResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,9 +14,19 @@ import java.util.List;
 public class CourseController {
     @Autowired
     private CourseRepository repository;
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PostMapping
+    public void saveCourse(@RequestBody CourseRequestDTO data){
+        Course courseData = new Course(data);
+        repository.save(courseData);
+        return;
+    }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping
-    public List<Course> getAll(){
-        List<Course> courseList = repository.findAll();
+    public List<CourseResponseDTO> getAll(){
+        List<CourseResponseDTO> courseList = repository.findAll().stream().map(CourseResponseDTO::new).toList();
         return courseList;
     }
 }
